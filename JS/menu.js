3168c8a2600767bba8edb0e72857e99b7f4a31f3
Hajar-Raguiti <raguitihajar@gmail.com>
2026-05-02 14:48:37 +0100

    Add menu toggle to header , creat a js script linked to all pages.

---
+++ JS/menu.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector('#nav-links');
+
+menu.addEventListener('click', function() {
+    navLinks.classList.toggle('active') });
